b66160117b8e9f1282cbc9efab3a89e7b25ae15f
6arms1leg <m.brnsfld@googlemail.com>
2021-09-26 18:51:12 +0200

    Use macros for reoccurring printed strings

--- ex-app/main.c
+++ ex-app/main.c
@@ -6,54 +6,59 @@
                               application/project-specific interface */
 #include "bufferObject.h" /* Include application interfaces */
 
+/* Reoccurring strings */
+#define APPPROMPT "CirQu ex-app:  "
+#define CNTMSG "Count of free element slots in `buf0`: "
+#define PULLMSG "Pulled (tail) element from `buf0`: "
+
 int MAIN(void)
 {
     /* Temporary storage for peeked/pulled CirQu element */
     static cirquElem_t cirquElem_elem = 'z';
 
-    puts("CirQu: ~~ Welcome to the CirQu buffer example application! ~~");
+    puts(APPPROMPT "~~ Welcome to the CirQu buffer example application! ~~");
 
-    puts("CirQu: Construct CirQu buffer `buf0` ...");
+    puts(APPPROMPT "Construct CirQu buffer `buf0` ...");
     fn_bffrObj_buf0Ctor(); /* Initialize CirQu buffer object "buffer 0" */
 
-    printf( "CirQu: Count of free element slots in `buf0`: %i\n",
+    printf( APPPROMPT CNTMSG "%i\n",
             (int)fn_bffr_cntFree(p_stc_bffr_g_buf0) );
 
-    puts("CirQu: Push elements `a`, `b`, `c` to `buf0` ...");
+    puts(APPPROMPT "Push elements `a`, `b`, `c` to `buf0` ...");
     fn_bffr_push(p_stc_bffr_g_buf0, (cirquElem_t)'a');
     fn_bffr_push(p_stc_bffr_g_buf0, (cirquElem_t)'b');
     fn_bffr_push(p_stc_bffr_g_buf0, (cirquElem_t)'c');
 
-    printf( "CirQu: Count of free element slots in `buf0`: %i\n",
+    printf( APPPROMPT CNTMSG "%i\n",
             (int)fn_bffr_cntFree(p_stc_bffr_g_buf0) );
 
     (void)fn_bffr_peek(p_stc_bffr_g_buf0, &cirquElem_elem, (cirquElemIdx_t)1U);
-    printf("CirQu: Peeked tail+1 element in `buf0`: %c\n",
+    printf(APPPROMPT "Peeked tail+1 element in `buf0`: %c\n",
            (char)cirquElem_elem);
 
-    printf( "CirQu: Count of free element slots in `buf0`: %i\n",
+    printf( APPPROMPT CNTMSG "%i\n",
             (int)fn_bffr_cntFree(p_stc_bffr_g_buf0) );
 
-    puts("CirQu: Push element `d` to `buf0` ...");
+    puts(APPPROMPT "Push element `d` to `buf0` ...");
     fn_bffr_push(p_stc_bffr_g_buf0, (cirquElem_t)'d');
 
-    printf( "CirQu: Count of free element slots in `buf0`: %i\n",
+    printf( APPPROMPT CNTMSG "%i\n",
             (int)fn_bffr_cntFree(p_stc_bffr_g_buf0) );
 
     (void)fn_bffr_pull(p_stc_bffr_g_buf0, &cirquElem_elem);
-    printf("CirQu: Pulled (tail) element from `buf0`: %c\n",
+    printf(APPPROMPT PULLMSG "%c\n",
            (char)cirquElem_elem);
     (void)fn_bffr_pull(p_stc_bffr_g_buf0, &cirquElem_elem);
-    printf("CirQu: Pulled (tail) element from `buf0`: %c\n",
+    printf(APPPROMPT PULLMSG "%c\n",
            (char)cirquElem_elem);
     (void)fn_bffr_pull(p_stc_bffr_g_buf0, &cirquElem_elem);
-    printf("CirQu: Pulled (tail) element from `buf0`: %c\n",
+    printf(APPPROMPT PULLMSG "%c\n",
            (char)cirquElem_elem);
 
-    printf( "CirQu: Count of free element slots in `buf0`: %i\n",
+    printf( APPPROMPT CNTMSG "%i\n",
             (int)fn_bffr_cntFree(p_stc_bffr_g_buf0) );
 
-    puts("CirQu: ~~ Bye! ~~");
+    puts(APPPROMPT "~~ Bye! ~~");
 
     return( (int)0 );
 }
