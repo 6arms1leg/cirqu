9ac626b18d49c6e325951fc566f1ce5c26b4b8ee
6arms1leg <m.brnsfld@googlemail.com>
2021-12-30 13:35:49 +0100

    Fix comment

--- test/support/app/bufferObject.h
+++ test/support/app/bufferObject.h
@@ -12,11 +12,9 @@
  */
 
 #define BUF0STRGSIZE ( (cirquElemIdx_t)4U )
-#define BUF0ELEMSIZE (BUF0STRGSIZE - (cirquElemIdx_t)1U) /* *-1*, because one
-                                                            element slot is use
-                                                            to distinguish full
-                                                            and empty fill
-                                                            level. */
+#define BUF0ELEMSIZE (BUF0STRGSIZE - (cirquElemIdx_t)1U)
+    /* `-1` because one element slot is used to distinguish full and empty
+       fill level */
 
 /* Global opaque pointer to buffer object "buffer 0" */
 extern stc_bffr_t* const p_stc_bffr_g_buf0;
