35b2de3285618810fc8eba3f313c4246780efae4
6arms1leg <m.brnsfld@googlemail.com>
2021-12-30 15:52:11 +0100

    Fix type prefix in name

--- test/support/app/bufferObject.c
+++ test/support/app/bufferObject.c
@@ -15,7 +15,7 @@ static stc_bffr_t stc_bffr_pv_buf0;
 stc_bffr_t* const p_stc_bffr_g_buf0 = &stc_bffr_pv_buf0;
 
 /* Private storage for buffer object "buffer 0" */
-static cirquElem_t a_cirquElem_buf0Strg[BUF0STRGSIZE] = { (cirquElem_t)0U };
+static cirquElem_t a_cirquElem_pv_buf0Strg[BUF0STRGSIZE] = { (cirquElem_t)0U };
 
 /* OPERATIONS
  * ==========
@@ -23,7 +23,7 @@ static cirquElem_t a_cirquElem_buf0Strg[BUF0STRGSIZE] = { (cirquElem_t)0U };
 
 void fn_bffrObj_buf0Ctor(void)
 {
-    fn_bffr_ini(&stc_bffr_pv_buf0, a_cirquElem_buf0Strg,
+    fn_bffr_ini(&stc_bffr_pv_buf0, a_cirquElem_pv_buf0Strg,
                 (cirquElemIdx_t)BUF0STRGSIZE);
 
     return;
