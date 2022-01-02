8d22cd7c0675f6cf7da7b7f279c76f95f538b007
6arms1leg <m.brnsfld@googlemail.com>
2022-01-02 15:14:15 +0100

    Fix wording

--- test/support/app/cirquElemType.h
+++ test/support/app/cirquElemType.h
@@ -5,8 +5,8 @@
 
 #include <stdint.h>
 
-#define CIRQUELEMQUAL_T /* Optional type qualifier, e.g. `volatile`. Empty if
-                           unused (but must be defined). */
+#define CIRQUELEMQUAL_T /* Optional type qualifier(s), e.g. `volatile`. Empty
+                           if unused (but must be defined). */
 typedef uint8_t cirquElem_t;
 typedef uint8_t cirquElemIdx_t; /* Must be unsigned integer, i.e. `uint8_t`,
                                    `uint16_t`, ... */
