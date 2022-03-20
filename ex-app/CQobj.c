e8632c16b2cda51a6c44b26777a549aed4235218
6arms1leg <m.brnsfld@googlemail.com>
2022-03-20 20:39:29 +0100

    Move buffers for queue objects to local scope
    This gives compliance with MISRA C:2012 rule 8.9.

--- ex-app/CQobj.c
+++ ex-app/CQobj.c
@@ -16,23 +16,26 @@ CQqu_qu_t0* const CQobj_p_qu0Alpha = &pv_qu0Alpha;
 CQqu_qu_t0* const CQobj_p_qu0Beta = &pv_qu0Beta;
 CQqu_qu_t1* const CQobj_p_qu1 = &pv_qu1;
 
-/* Private buffers for queue objects */
-static CQqu_item_t0 pv_buf0Alpha[CQOBJ_BUF0ALPHA_SIZ];
-static CQqu_item_t0 pv_buf0Beta[CQOBJ_BUF0BETA_SIZ];
-static CQqu_item_t1 pv_buf1[CQOBJ_BUF1_SIZ];
-
 /* OPERATIONS
  * ==========
  */
 
 void CQobj_qu0AlphaCtor(void) {
-    CQqu_init0(&pv_qu0Alpha, pv_buf0Alpha, CQOBJ_BUF0ALPHA_SIZ);
+    static CQqu_item_t0 buf0Alpha[CQOBJ_BUF0ALPHA_SIZ]; /* Buffer for queue
+                                                           object */
+
+    CQqu_init0(&pv_qu0Alpha, buf0Alpha, CQOBJ_BUF0ALPHA_SIZ);
 }
 
 void CQobj_qu0BetaCtor(void) {
-    CQqu_init0(&pv_qu0Beta, pv_buf0Beta, CQOBJ_BUF0BETA_SIZ);
+    static CQqu_item_t0 buf0Beta[CQOBJ_BUF0BETA_SIZ]; /* Buffer for queue
+                                                         object */
+
+    CQqu_init0(&pv_qu0Beta, buf0Beta, CQOBJ_BUF0BETA_SIZ);
 }
 
 void CQobj_qu1Ctor(void) {
-    CQqu_init1(&pv_qu1, pv_buf1, CQOBJ_BUF1_SIZ);
+    static CQqu_item_t1 buf1[CQOBJ_BUF1_SIZ]; /* Buffer for queue object */
+
+    CQqu_init1(&pv_qu1, buf1, CQOBJ_BUF1_SIZ);
 }
--- test/support/CQobj.c
+++ test/support/CQobj.c
@@ -12,13 +12,12 @@ static CQqu_qu_t0 pv_qu0;
 /* Global opaque pointer to queue object */
 CQqu_qu_t0* const CQobj_p_qu0 = &pv_qu0;
 
-/* Private buffer for queue object */
-static CQqu_item_t0 pv_buf0[CQOBJ_BUF0_SIZ];
-
 /* OPERATIONS
  * ==========
  */
 
 void CQobj_qu0Ctor(void) {
-    CQqu_init0(&pv_qu0, pv_buf0, CQOBJ_BUF0_SIZ);
+    static CQqu_item_t0 buf0[CQOBJ_BUF0_SIZ]; /* Buffer for queue object */
+
+    CQqu_init0(&pv_qu0, buf0, CQOBJ_BUF0_SIZ);
 }
